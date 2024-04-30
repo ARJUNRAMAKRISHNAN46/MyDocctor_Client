3d114b870bbb30564861a1586239416978adf3e7
ARJUN P <arjunpchr@gmail.com>
2024-04-30 17:28:59 +0530

    commit 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Router from './components/Router'
-import Test from "./pages/Test";
 
 function App() {
   return (
     <>
-      {/* <Router/> */}
-      <Test/>
+      <Router/>
     </>
   );
 }
--- src/pages/mode-changer.tsx
+++
@@ -1,8 +0,0 @@
-import { Moon, Sun } from 'lucide-react';
-import { useTheme } from '../components/common/theme-provider';
-
-export function modeChanger () {
-    const{setTheme} = useTheme();
-
-    return 
-}
