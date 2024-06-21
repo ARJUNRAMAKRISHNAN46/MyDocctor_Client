cfffa13b37d8af0f55bfc9be7354b4273099ded7
ARJUN P <arjunpchr@gmail.com>
2024-06-21 10:23:45 +0530

    update: admin doctor listing table

--- src/components/admin/AdminDoctors.tsx
+++ src/components/admin/AdminDoctors.tsx
@@ -15,11 +15,7 @@ function AdminDoctors() {
     dispatch(listDoctor())
       .then((res) => {
         setDoctors(res.payload.data);
-        console.log("🚀 ~ dispatch ~ doctor ~ res:", res.payload.data);
       })
-      .catch((err) => {
-        console.log("🚀 ~ dispatch ~ err:", err);
-      });
   }, [dispatch, status]);
 
   const handleNavigate = (doctor_id: any) => {
@@ -30,12 +26,8 @@ function AdminDoctors() {
   const handleAction = (id: string) => {
     dispatch(blockUser(id))
       .then((res) => {
-        console.log("🚀 ~ dispatch ~ res:", res);
         setStatus(!status)
       })
-      .catch((err) => {
-        console.log("🚀 ~ dispatch ~ err:", err);
-      });
   };
 
   return (
@@ -44,54 +36,54 @@ function AdminDoctors() {
         <div className="flex items-center py-4">
           <h1 className="text-white font-bold">ADMIN</h1>
           <IoIosArrowForward className="text-xl text-white" />
-          <h1 className="text-white font-bold">DOCTORS</h1>
+          <h1 className="text-gray-200 font-bold">DOCTORS</h1>
         </div>
         <div className="flex">
-          <div className="border w-[5%]  border-gray-500 text-center">
+          <div className="border-y w-[60px]  border-gray-500 text-center text-sm">
             <h1 className="text-white py-2 font-bold">Sl.No</h1>
           </div>
-          <div className="border w-[23%] py-2 border-gray-500 text-center">
-            <h1 className="text-white font-bold">Doctor Name</h1>
+          <div className="border-y w-[300px] py-2 border-gray-500 text-center text-sm">
+            <h1 className="text-gray-200 font-bold">Doctor Name</h1>
           </div>
-          <div className="border w-[25%] py-2 border-gray-500 text-center">
-            <h1 className="text-white font-bold">Doctor ID</h1>
+          <div className="border-y w-[300px] py-2 border-gray-500 text-center text-sm">
+            <h1 className="text-gray-200 font-bold">Doctor ID</h1>
           </div>
-          <div className="border w-[27%] py-2 border-gray-500 text-center">
-            <h1 className="text-white font-bold">Doctor Email</h1>
+          <div className="border-y w-[350px] py-2 border-gray-500 text-center text-sm">
+            <h1 className="text-gray-200 font-bold">Doctor Email</h1>
           </div>
-          <div className="border w-[10%] py-2 border-gray-500 text-center">
-            <h1 className="text-white font-bold">Action</h1>
+          <div className="border-y w-[150px] py-2 border-gray-500 text-center text-sm">
+            <h1 className="text-gray-200 font-bold">Action</h1>
           </div>
-          <div className="border w-[10%] py-2 border-gray-500 text-center">
-            <h1 className="text-white font-bold">View</h1>
+          <div className="border-y w-[150px] py-2 border-gray-500 text-center text-sm">
+            <h1 className="text-gray-200 font-bold">View</h1>
           </div>
         </div>
         {doctors.map((data: any, index) => (
           <div className="flex">
-            <div className="border w-[5%]  border-gray-500 text-center py-2">
-              <h1 className="text-white">{index + 1}</h1>
+            <div className="border-b w-[60px]  border-gray-500 text-center py-2.5">
+              <h1 className="text-gray-100 text-sm font-thin">{index + 1}</h1>
             </div>
-            <div className="border w-[23%] border-gray-500 text-center py-2">
-              <h1 className="text-white">{data.name}</h1>
+            <div className="border-b w-[300px] border-gray-500 text-center py-2.5">
+              <h1 className="text-gray-100 text-sm font-thin">{data.name}</h1>
             </div>
-            <div className="border w-[25%] border-gray-500 text-center py-2">
-              <h1 className="text-white">{data._id}</h1>
+            <div className="border-b w-[300px] border-gray-500 text-center py-2.5">
+              <h1 className="text-gray-100 text-sm font-thin">{data._id}</h1>
             </div>
-            <div className="border w-[27%] border-gray-500 text-center py-2">
-              <h1 className="text-white">{data?.email}</h1>
+            <div className="border-b w-[350px] border-gray-500 text-center py-2.5">
+              <h1 className="text-gray-100 text-sm font-thin">{data?.email}</h1>
             </div>
-            <div className="border w-[10%] border-gray-500 text-center py-2">
+            <div className="border-b w-[150px] border-gray-500 text-center py-2.5">
               <button
                 onClick={() => handleAction(data?._id)}
-                className={`${data?.isActive === true ? "bg-green-600" : "bg-red-600"} text-white bg-blue-600 px-4 rounded-[5px]`}
+                className={`${data?.isActive === true ? "bg-green-600" : "bg-red-600"} text-wh1te text-sm font-thin bg-blue-600 px-4 rounded-[5px]`}
                 >
               {data?.isActive === true ? "Active" : "Blocked"}
               </button>
             </div>
-            <div className="border w-[10%] border-gray-500 text-center py-2">
+            <div className="border-b w-[150px] border-gray-500 text-center py-2.5">
               <button
                 onClick={() => handleNavigate(data?._id)}
-                className="text-white bg-blue-600 px-6 rounded-[5px]"
+                className="text-wh1te text-sm font-thin bg-blue-600 px-6 rounded-[5px]"
               >
                 View
               </button>
