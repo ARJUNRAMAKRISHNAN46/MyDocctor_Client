b481f53eccbb38f142dd15a8ec73b48a3ce6fb22
ARJUN P <arjunpchr@gmail.com>
2024-07-14 13:05:45 +0530

    refactor: backend link changed

--- src/components/doctor/SearchDoctor.tsx
+++ src/components/doctor/SearchDoctor.tsx
@@ -28,7 +28,7 @@ const SearchDoctors: React.FC = () => {
       if (value.length > 1) {
         try {
           const response: any = await axios.get<Doctor[]>(
-            `https://mydocctor.online/doctor/api/search-doctors?q=${value}`
+            `https://mydocctor.online/api/doctor/search-doctors?q=${value}`
           );
           console.log("🚀 ~ handleSearch ~ response:", response.data?.data);
           setDoctors(response.data?.data);
