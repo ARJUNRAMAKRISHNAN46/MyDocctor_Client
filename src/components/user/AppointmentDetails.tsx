6011b93b2629fd0cd6440d4b6d6c03a708227b5f
ARJUN P <arjunpchr@gmail.com>
2024-07-22 10:29:05 +0530

    refactor: appointment details page

--- src/components/user/AppointmentDetails.tsx
+++ src/components/user/AppointmentDetails.tsx
@@ -1,32 +1,56 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 const AppointmentDetails = () => {
   const { id } = useParams();
+  const [slotData, setSlotData] = useState<any>();
+  const [paymentData, setPaymentData] = useState<any>();
   function useQuery() {
     return new URLSearchParams(useLocation().search);
   }
   const query = useQuery();
-  const doctorName = query.get('doctorName');
-  console.log("🚀 ~ AppointmentDetails ~ doctorName:", doctorName)
+  const doctorName = query.get("doctorName");
+  console.log("🚀 ~ AppointmentDetails ~ doctorName:", doctorName);
   console.log("🚀 ~ AppointmentDetails ~ id:", id);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const response = await axios.get(
-      // `http://localhost:8080/appointment/api/get-slot/${id}`
       `https://mydocctor.online/api/appointment/get-slot/${id}`
     );
-    // const payment = await axios.get(`https://mydocctor.online/api/payments/find-payment/`)
+    const payment = await axios.get(
+      `https://mydocctor.online/api/payments/find-payment/${response.data?.data?.slots[0].paymentId}`
+    );
+    setPaymentData(payment?.data?.data);
     console.log("response.data: ", response.data?.data);
-    console.log("response.data payment id: ", response.data?.data?.slots[0].paymentId);
+    setSlotData(response.data?.data);
+    console.log("🚀 ~ fetchData ~ payment:", payment);
+    console.log(
+      "response.data payment id: ",
+      response.data?.data?.slots[0].paymentId
+    );
   };
   return (
     <div className="w-[82vw] h-[96vh] bg-white">
-      <div>
-       
+      <div className="md:px-20 py-10">
+        <h1 className="text-2xl font-semibold text-blue-900 text-center">YOUR BOOKING DETAILS</h1>
+        <h1 className="text-center font-semibold">
+          DOCTOR NAME: <span>{doctorName}</span>
+        </h1>
+        <h1 className="text-center font-semibold">
+          SLOT TIME: <span>{slotData?.slots[0]?.start}</span>
+        </h1>
+        <h1 className="text-center font-semibold">
+          SLOT DATE: <span>{slotData?.date}</span>
+        </h1>
+        <h1 className="text-center font-semibold">
+          FEES: <span>{paymentData?.fees}</span>
+        </h1>
+        <h1 className="text-center font-semibold">
+          PAYMENT DATE: <span>{paymentData?.createdAt?.toLocaleDateString()}</span>
+        </h1>
       </div>
     </div>
   );
